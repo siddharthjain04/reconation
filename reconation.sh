0c2b3717a4545bc852244e4058528b0d4b5cc99c
siddharthjain04 <103101615+siddharthjain04@users.noreply.github.com>
2022-05-19 17:15:33 +0530

    Update reconation.sh

--- reconation.sh
+++ reconation.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ -d ~/siddharth/recon/$1/$(date +"%d-%m-%Y") ]
-then
-        echo -e "\e[1;33m[+] Direcotry already exists\e[0m"
-else
-        mkdir -p ~/siddharth/recon/$1/$(date +"%d-%m-%Y")
-        echo -e "\e[1;33m[+] Direcotry Created\e[0m"
-fi
-
-cd ~/siddharth/recon/$1/$(date +"%d-%m-%Y")
+cd /var/www/html/
 
 echo -e "\e[1;33m[+] Running certspotter & CRTsh\e[0m"
 curl -s https://crt.sh/?q=%25.$1 | grep $1 | grep "<TD>" | cut -d">" -f2 | cut -d"<" -f1 | sort -u | sed s/*.//g >> crtsh.txt
@@ -48,25 +40,16 @@ echo -e "\e[1;33m[+] Running WaybackUrl | GAU | Hakrawler\e[0m"
 cat live_domains.txt | waybackurls | tee tmp-url.txt
 echo $1 | gau >> tmp-url.txt
 echo $1 | hakrawler | awk '{print $2}' >> tmp-url.txt
-cd /root/tools/waybackMachine
-python waybackmachine.py $1 >> tmp-url
-cd ~/siddharth/recon/$1/$(date +"%d-%m-%Y")
+cd /home/ubuntu/tools/waybackMachine
+python waybackmachine.py $1 >> /var/www/html/tmp-url
+cd /var/www/html/
 sort -u tmp-url.txt >>  tmp.txt
 cat tmp.txt | egrep -v "\.woff|\.ttf|\.svg|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico" | sed 's/:80//g;s/:443//g' | sort -u > urls.txt
 
 
 echo -e "\e[1;33m[+] Running FFUF\e[0m"
-ffuf -c -u "FUZZ" -w urls.txt -of csv -o valid-tmp.txt
-cat valid-tmp.txt | grep http | awk -F "," '{print $1}' >> valid-urls.txt
-
-echo -e "\e[1;33m[+] Running GF\e[0m"
-gf xss valid-urls.txt | tee gf_xss.txt
-gf sqli valid-urls.txt | tee gf_sql.txt
-gf lfi valid-urls.txt | tee gf_lfi.txt 
-gf rce valid-urls.txt | tee gf_rce.txt
-gf idor valid-urls.txt | tee gf_idor.txt
-gf redirect valid-urls.txt | tee gf_redirect.txt
-gf ssrf valid-urls.txt | tee gf_ssrf.txt
+ffuf -c -u "FUZZ" -w urls.txt -of csv -o /var/www/html/valid-tmp.txt
+cat valid-tmp.txt | grep http | awk -F "," '{print $1}' >> /var/www/html/valid-urls.txt
 
 echo -e "\e[1;33m[+]Finding all the Live IP's\e[0m"
 ipfinder live_domains.txt ipfinder.txt
@@ -82,20 +65,17 @@ echo -e "\e[1;33m[+]Cleaning IP list 's\e[0m"
 sort -u tmp-ip.txt > ip.txt
 
 echo -e "\e[1;33m[+]Running Port Scan 's\e[0m" 
-naabu -hL ip.txt -o live_ip.txt -ports full -silent
-
-echo -e "\e[1;33m[+]Running Broken Link Checker 's\e[0m" 
-blc $1 -ro -v > Bronke-link-checker.txt
+naabu -hL ip.txt -o /var/www/html/live_ip.txt -ports full -silent
 
 echo -e "\e[1;33m[+]Running ParamSpider 's\e[0m" 
-cd /root/tools/ParamSpider/
-python3 paramspider.py -d $1 -o ~/siddharth/recon/$1/$(date +"%d-%m-%Y")/paramspider.txt 
-cd ~/siddharth/recon/$1/$(date +"%d-%m-%Y")
+cd /home/ubuntu/tools/ParamSpider/
+python3 paramspider.py -d $1 -o /var/www/html/paramspider.txt 
+cd /var/www/html/
 
 echo -e "\e[1;33m[+]Running FFUF 's\e[0m" 
-ffuf -c -w /root/massdns/lists/all.txt -u $1/FUZZ -mc all -fs 4242 -v -o fuff.txt 
+ffuf -c -w /home/ubuntu/massdns/lists/all.txt -u $1/FUZZ -mc all -fs 4242 -v -o /var/www/html/fuff.txt 
 
 echo -e "\e[1;33m[+]Running Arjun 's\e[0m" 
-cd /root/tools/Arjun/
-python3 arjun.py --urls valid-urls.txt --get -o arjun.txt
+cd /home/ubuntu/tools/Arjun/
+python3 arjun.py --urls valid-urls.txt --get -o /var/www/html/arjun.txt
 
